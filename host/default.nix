85ec6b63fee9d7a7b28b5a8d9d4696b0ed9da01c
tunitz <labillar@tunitz.com>
2026-08-13 00:31:37 +0800

    host default

--- host/default.nix
+++ host/default.nix
@@ -1,20 +1,36 @@
-{ ... }:
+/**
+  Default configuration for user related configs.
+  No need to modify this file, instead, create a directory with a default.nix file where you can customize as you want per host.
+*/
+{ lib, host, ... }:
 
 {
   # Region
-  time.timeZone = "Asia/Manila";
+  time.timeZone = lib.mkDefault "Asia/Manila";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_PH.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "fil_PH";
-    LC_IDENTIFICATION = "fil_PH";
-    LC_MEASUREMENT = "fil_PH";
-    LC_MONETARY = "fil_PH";
-    LC_NAME = "fil_PH";
-    LC_NUMERIC = "fil_PH";
-    LC_PAPER = "fil_PH";
-    LC_TELEPHONE = "fil_PH";
-    LC_TIME = "fil_PH";
+  i18n = lib.mkDefault {
+    defaultLocale = "en_PH.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "fil_PH";
+      LC_IDENTIFICATION = "fil_PH";
+      LC_MEASUREMENT = "fil_PH";
+      LC_MONETARY = "fil_PH";
+      LC_NAME = "fil_PH";
+      LC_NUMERIC = "fil_PH";
+      LC_PAPER = "fil_PH";
+      LC_TELEPHONE = "fil_PH";
+      LC_TIME = "fil_PH";
+    };
+  };
+
+  # Default hostname
+  networking.hostName = lib.mkDefault host;
+
+  # Default user
+  users.users.${host} = lib.mkDefault {
+    isNormalUser = true;
+    description = "${host}";
+    extraGroups = [ "networkmanager" "wheel" ];
   };
 }
