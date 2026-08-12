ff8c27e26537c94b8e66097b8ff6d80f3531d3a8
tunitz <labillar@tunitz.com>
2026-08-12 22:52:44 +0800

    remove gamescope

--- system/tunitz/steam.nix
+++ system/tunitz/steam.nix
@@ -5,8 +5,8 @@
     enable = true;
     remotePlay.openFirewall = true; 
     dedicatedServer.openFirewall = true;
-    gamescopeSession.enable = true;
+    # gamescopeSession.enable = true;
   };
 
-  programs.gamescope.enable = true;
+  # programs.gamescope.enable = true;
 }
